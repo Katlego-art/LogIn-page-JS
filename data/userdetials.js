7e21482a571fcd4137a9c1f52fead3608c476e30
Kat <142260481+Katlego-art@users.noreply.github.com>
2025-01-01 23:19:20 +0200

    added user login details

--- data/userdetials.js
+++ data/userdetials.js
@@ -1,6 +1,22 @@
-const userDetials = [{
-  id: "",
-  name:"",
-  emial: ""
-}];
-
+export const userDetials = [
+  {
+    id: "dhfbhfksdfbsd-dfhdfhkd-jdfjd",
+    name: "Katlego M",
+    username: "kat@gmail.com",
+    emial: "kat@gmail.com",
+    password: "df8asdv7uasdh@sd",
+  },
+  {
+    id: "djhabsdyia--shsaj6",
+    name: "John willson",
+    username: "willson@gmail.com",
+    email: "willson@gmail.com",
+    password: "sd6vshdvs9gvga%$",
+  },{
+    id: "hdima-jsbd8sahdbha",
+    name: "Koketso black",
+    username: "koketso@gmail.com",
+    email: "koketso@gmail.com",
+    password: "shv8sdusdnh&bsdh**"
+  }
+];
---
+++ login.js
@@ -0,0 +1,16 @@
+import { userDetials } from "./data/userdetials.js";
+
+const userName = document.querySelector(".js-username-input").value;
+
+const passWord = document.querySelector(".js-password-input").value;
+
+document.querySelector(".js-login-button").addEventListener("click", () => {
+
+  
+
+
+
+ 
+
+
+});
